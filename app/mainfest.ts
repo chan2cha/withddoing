4c06b715586ca7db498a7332acbae32b59f27b2c
chan2cha <kmongside91@gmail.com>
2026-03-09 17:19:17 +0900

    feat: logo 추가

--- app/mainfest.ts
+++ app/mainfest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#0EA5E9",
     icons: [
       {
-        src: "/icons/icon-192.png",
+        src: "/public/ddoing.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icons/icon-512.png",
+        src: "/public/ddoing.png",
         sizes: "512x512",
         type: "image/png",
       },
